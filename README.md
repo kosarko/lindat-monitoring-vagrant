3db968793ca53adcc897f279895dfd2a27b46916
Ondřej Košarko <kosarko@ufal.mff.cuni.cz>
2015-12-07 12:12:34 +0100

    setup.sh pull lindat-monitoring

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # lindat-monitoring-vagrant
+Spins up a machine from https://github.com/Icinga/icinga-vagrant, pulls and sets up https://github.com/ufal/lindat-monitoring in that machine.
+
+## Running
+```
+cd icinga2x
+vagrant up
+```
+
+http://localhost:8082 should take you to icingaweb2 UI
+
+see icinga2x/README for more details
---
+++ icinga2x/monitoring/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#install prerequisites
+yum install -q -y perl-libwww-perl perl-switch perl-LWP-Protocol-https
+pushd /tmp
+wget -O "html-xml-utils.rpm" "ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/TrevorBl/CentOS_CentOS-6/x86_64/html-xml-utils-6.9-4.1.x86_64.rpm" && yum install -q -y html-xml-utils.rpm
+popd
+
+#git clone
+REPO_URL=https://github.com/kosarko/lindat-monitoring.git
+pushd /opt
+git clone $REPO_URL lindat-monitoring
+echo 'include_recursive "/opt/lindat-monitoring/ufal.d"' >> /etc/icinga2/icinga2.conf
+echo 'const LindatPluginDir = "/opt/lindat-monitoring/plugins"' >> /etc/icinga2/conf.d/constants.conf
+popd
+
+#restart
+systemctl restart icinga2
